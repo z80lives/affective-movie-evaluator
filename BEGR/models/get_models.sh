3823e03264b86fbf11a7422d6c15d2e4d7edeba5
Shath Ibrahim <shath.ibrahim@gmail.com>
2019-03-26 12:22:53 +0800

    recovered broken BEGR code

---
+++ BEGR/models/get_models.sh
@@ -0,0 +1,2 @@
+wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel
+wget https://raw.githubusercontent.com/CMU-Perceptual-Computing-Lab/openpose/master/models/pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt
